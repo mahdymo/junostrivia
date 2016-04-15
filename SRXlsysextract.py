11427994f94b69411a081d6f9d3a6fe1fc87264f
Mahdy <mohamed.mahdy4@gmail.com>
2016-04-15 12:49:44 +0200

    Added files via upload
    This file is to make it easier to extract info from SRX with multiple LSYS enabled.

---
+++ SRXlsysextract.py
@@ -0,0 +1,152 @@
+__author__ = 'MMahdy'
+########################################################################################################################
+######################This project aims to ease the extraction of zones, ###############################################
+######################################### interfacesrelated to certain IPs from SRX with Logical system enabled#########
+########################################################################################################################
+#Import list
+import paramiko
+import csv
+import pyexcel as pe
+
+###################################################################################################################
+################### Functions creation################################
+####################################################################################################################
+##Check interfaces
+def Interface(x):
+    datain, dataout, dataerr = SRX.exec_command(
+        'show route forwarding-table matching ' + x + ' extensive | match "Next-hop interface" | trim 22')
+    intf = dataout.read()
+    return(intf)
+
+##Zone info for zones at the internal side (Services side)requires IP as input
+def Zone(y):
+    datain, dataout, dataerr = SRX.exec_command("show interface " + y + " | match zone | trim 20 ")
+    zonetmp = dataout.read()
+    zonetmp = zonetmp[:zonetmp.index('\n')]
+    return(zonetmp)
+
+##Logical system information requires IP as input
+def LSYS(x):
+    index = 0
+    index1 = 0
+    datain, dataout1, dataerr = SRX.exec_command(
+        "show route forwarding-table matching " + x + " extensive ")
+    intf = dataout1.readlines()
+    for i in intf:
+        if "Destination" in i:
+            index = intf.index(i)
+        index1 = index - 4
+
+    LSYStemp = intf[index1]
+    temp =  LSYStemp[LSYStemp.index(': '):]
+    LSYS = temp[temp.index(' '):]
+    return (LSYS)
+
+##provide logical system and interface requires IP as input
+def RoutingTable(x):
+    datain, dataout1, dataerr = SRX.exec_command(
+        "show route forwarding-table matching " + x + " extensive ")
+    intf = dataout1.readlines()
+    for i in intf:
+        if "Destination" in i:
+            index= intf.index(i)
+            index1 = index -3
+
+    Rinsttemp = intf[index1]
+    temp =  Rinsttemp[Rinsttemp.index(': '):Rinsttemp.index('.inet')]
+    Rinst = temp[temp.index(' '):]
+    return(Rinst)
+
+##Checking the external interfaces (out towards PE side) requires (Logical system, routing instance, IP)
+def ExternalIntf(x,y,z):
+    datain, dataout, dataerr = SRX.exec_command("show route logical-system " + x + " table " + y +" " +z +
+                                                " | match via | trim 40")
+    Exint = dataout.read()
+    Exint = Exint[Exint.index(' '):Exint.index('\n')]
+    return(Exint)
+
+########################********************#############################**************###############################
+########################################Program ********************* Program ########################################
+########################********************#############################**************###############################
+
+file = raw_input("Please enter the source file containing the source and destination IPs: ")
+Ofile = raw_input("Please enter the Destination file to send the output: ")
+UN = raw_input("Please enter the user name: ")
+PWD = raw_input("Please enter the password: ")
+SourceIP = []
+DestinationIP = []
+i = 0
+SourceInterface = []
+SourceZone = []
+LogicalSystem = []
+RoutingInstance = []
+DestinationInterface =[]
+DestinationZone = []
+Check = 'Y'
+while Check == 'Y':
+    device = raw_input("Please enter the Firewall IP to be checked: ")
+    SRX = paramiko.SSHClient()
+    SRX.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    SRX.connect(device, username=UN, password=PWD)
+
+    with open(file, 'r') as f:
+        reader = csv.reader(f)
+        next(reader)
+        for row in reader:
+            SourceIP.append(row[0])
+            DestinationIP.append(row[1])
+
+
+    while i < len(SourceIP):
+        Sintf = Interface(SourceIP[i])
+        if Sintf.find('reth') != -1:
+            SourceInterface.append(Sintf)
+            lsys = LSYS(SourceIP[i])
+            LogicalSystem.append(lsys)
+
+        else:
+            tmp = Interface(DestinationIP[i])
+            lsys = LSYS(DestinationIP[i])
+            LogicalSystem.append(i)
+            rt = RoutingTable(DestinationIP[i])
+            RoutingInstance.append(rt)
+            eXInterface = ExternalIntf(lsys, rt, SourceIP[i])
+            SourceInterface.append(eXInterface)
+        i += 1
+
+    i = 0
+    while i < len(DestinationIP):
+        Dintf = Interface(DestinationIP[i])
+        if Dintf.find('reth') != -1:
+            DestinationInterface.append(Dintf)
+        else:
+            tmp = Interface(SourceIP[i])
+            lsys = LSYS(SourceIP[i])
+            rt = RoutingTable(SourceIP[i])
+            eXInterface = ExternalIntf(lsys, rt, DestinationIP[i])
+            DestinationInterface.append(eXInterface)
+        i += 1
+
+
+    for Sintf in SourceInterface:
+            Szone = Zone(Sintf)
+            SourceZone.append(Szone)
+
+    for Dintf in DestinationInterface:
+        Dzone = Zone(Dintf)
+        DestinationZone.append(Dzone)
+
+
+    Transdata = []
+    row = 0
+    col = 0
+    data = []
+    data = [SourceZone,SourceIP,SourceInterface,DestinationZone,DestinationIP,DestinationInterface]
+    Transdata = zip(*data)
+    sheet = pe.Sheet(Transdata)
+    #print sheet
+    sheet.save_as(Ofile)
+    f.close()
+    SRX.close()
+    Check = raw_input("Start checking the Firewall now(Y or N): ")
+
