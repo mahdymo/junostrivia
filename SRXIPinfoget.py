312381d6ac09dad466092b36770d85455f0452e3
Mahdy <mohamed.mahdy4@gmail.com>
2016-11-25 04:30:37 +0300

    Update SRXIPinfoget.py
    updating the script for better functionality

--- SRXIPinfoget.py
+++ SRXIPinfoget.py
@@ -13,54 +13,34 @@ from threading import Thread
 ###################################################################################################################
 ################### Functions creation################################
 ####################################################################################################################
-##Check interfaces
-def Interface(x):
-    datain, dataout, dataerr = SRX.exec_command(
-        'show route forwarding-table matching ' + x + ' extensive | match "Next-hop interface" | trim 22')
-    intf = dataout.read()
-    return(intf)
+def Interface(k):
+        datain, dataout, dataerr = SRX.exec_command('show route forwarding-table matching ' + k + ' extensive | match "Next-hop interface" | trim 22')
+        return(dataout.read())
 
-##Zone info for zones at the internal side (Services side)requires IP as input
 def Zone(y):
     datain, dataout, dataerr = SRX.exec_command("show interface " + y + " | match zone | trim 20 ")
-    zonetmp = dataout.read()
-    zonetmp = zonetmp[:zonetmp.index('\n')]
-    return(zonetmp)
-
-##provide logical system and interface requires IP as input
-def RoutingTable(x):
-    datain, dataout1, dataerr = SRX.exec_command(
-        "show route forwarding-table matching " + x + " extensive ")
-    intf = dataout1.readlines()
-    for i in intf:
-        if "Destination" in i:
-            index= intf.index(i)
-            index1 = index -3
-
-    Rinsttemp = intf[index1]
-    temp =  Rinsttemp[Rinsttemp.index(': '):Rinsttemp.index('.inet')]
-    Rinst = temp[temp.index(' '):]
-    return(Rinst)
+    return(dataout.read())
 
 
-##Function used to handle the device related info, requires (Input file, output file, Username, password, device IP)
+
+##Function used to handle the device related info
 def SRXinfoextract(x):
-    SourceIP =[]
-    SourceInterface = []
-    SourceZone = []
-    RoutingInstance = []
-    sint = Interface(x)
-    SourceInterface.append(sint)
-    rt = RoutingTable(x)
-    RoutingInstance.append(rt)
-    SourceIP.append(x)
-    for sinf in SourceInterface:
-        Szone = Zone(sinf)
-        SourceZone.append(Szone)
+    SourceIP = x
+    SourceInterface = Interface(x)
+    if SourceInterface.strip()=="" :
+        print "Interface:"+"Null"+"\r\n"+"Zone:"+"Null"+"\r\n"
+    else:
+        SourceZone = Zone(SourceInterface)
+        print "Interface:"+SourceInterface+"Zone:"+SourceZone+"\r\n"
 
-    data = [SourceIP,"\t", SourceInterface, "\t", SourceZone,"\t", RoutingInstance]
 
-    return(data)
+   
+## Connection function
+def fwconnect(l):
+        SRX.connect(l, username=UN, password=PWD)
+        print l
+        SRXinfoextract(ip)
+        SRX.close()
 ########################********************#############################**************###############################
 ########################################Program ********************* Program ########################################
 ########################********************#############################**************###############################
@@ -69,19 +49,19 @@ Check = 'Y'
 ip = raw_input("Please enter the IP you want to check: ")
 UN = raw_input("Please enter the user name: ")
 PWD = getpass.getpass("Please enter the password: ")
-IP_LIST = ["10.7.37.140","10.7.37.188","10.7.38.108","10.7.37.228","10.7.37.196","10.7.38.132","10.7.37.252"]
+firewalls_list = raw_input("Please enter the file containing your firewalls: ")
+file = open(firewalls_list,"r")
+IP_LIST = file.readlines()
+IP_LIST = map(lambda s: s.strip(), IP_LIST)
+global SRX
 
-
-def fwconnect(l):
-    SRX = paramiko.SSHClient()
-    SRX.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    SRX.connect(l, username=UN, password=PWD)
-    srx_info = SRXinfoextract(ip)
-    SRX.close()
-    print l
-    print srx_info
-
-
-for l in IP_LIST:
-    th1 = Thread(target=fwconnect, args=(l,))
-    th1.start()
+while Check == "Y":
+        ip = raw_input("Please enter the IP you want to check: ")
+        SRX = paramiko.SSHClient()
+        SRX.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        for l in IP_LIST:
+                th1 = Thread(target=fwconnect, args=(l,))
+                th1.start()
+                time.sleep(4)   ## This sleep to leave time for ssh communication
+                                        ##if the delay is high in your network you can increase this sleep time
+        Check = raw_input("Do you want to check any other addresses(Y or N): ")
